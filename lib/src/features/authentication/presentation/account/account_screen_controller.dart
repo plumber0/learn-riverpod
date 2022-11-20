3c364388cfce2bbd23e3aa19ea15b5fec15ddc75
Seugwoo Yoo <seungwoo@deepnatural.ai>
2022-11-20 15:47:23 +0900

    The AsyncValue.guard method

--- lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -28,15 +28,9 @@ class AccountScreenController extends StateNotifier<AsyncValue<void>> {
     /// if success, set state to data
     /// if error, set state to error
 
-    try {
-      state = const AsyncValue<void>.loading();
-      await authRepository.signOut();
-      state = const AsyncValue<void>.data(null);
-      return true;
-    } catch (e, st) {
-      state = AsyncValue<void>.error(e, st);
-      return false;
-    }
+    state = const AsyncValue<void>.loading();
+    state = await AsyncValue.guard(() => authRepository.signOut());
+    return state.hasError == false;
 
     /// we're setting the state multiple times
     /// By 'watching' this StateNotifier in the build() method,
