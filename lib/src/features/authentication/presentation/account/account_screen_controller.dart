3f18aa8c4b5193f3284ce3530f1d08eccf6a11cd
Seugwoo Yoo <seungwoo@deepnatural.ai>
2022-11-20 13:11:02 +0900

    Creating our first controller using StateNotifier

---
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -0,0 +1,40 @@
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+/// StateNotifier is an alternative to ValueNotifier in the Flutter SDK
+/// Ideal for managing immutable state that widgets can listen to
+
+class AccountScreenController extends StateNotifier<AsyncValue<void>> {
+  /// AsyncValue.data needs a type
+  /// We can use <void> and (null) if we have "no value"
+  /// For our purposes, AsyncValue.data(null) means "not loading"
+  AccountScreenController({required this.authRepository})
+      : super(const AsyncValue<void>.data(null));
+  final AuthRepository authRepository;
+
+  Future<void> signOut() async {
+    /// set state to loading
+    /// sign out (using auth repository)
+    /// if success, set state to data
+    /// if error, set state to error
+
+    try {
+      state = const AsyncValue<void>.loading();
+      await authRepository.signOut();
+      state = const AsyncValue<void>.data(null);
+    } catch (e, st) {
+      state = AsyncValue<void>.error(e, st);
+    }
+
+    /// we're setting the state multiple times
+    /// By 'watching' this StateNotifier in the build() method,
+    /// our widget will rebuild every time the state changes
+  }
+}
+
+final accountScreenControllerProvider =
+    StateNotifierProvider.autoDispose<AccountScreenController, AsyncValue<void>>((ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return AccountScreenController(authRepository: authRepository);
+});
