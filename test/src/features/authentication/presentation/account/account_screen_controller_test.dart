cc37df2a0733247e6586c98c4f0af2a74cf11846
Seugwoo Yoo <seungwoo@deepnatural.ai>
2022-12-04 11:09:01 +0900

    Testing the AccountScreenController(part2) + working with mocks

--- test/src/features/authentication/presentation/account/account_screen_controller_test.dart
+++ test/src/features/authentication/presentation/account/account_screen_controller_test.dart
@@ -2,13 +2,26 @@ import 'package:ecommerce_app/src/features/authentication/data/fake_auth_reposit
 import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen_controller.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+/// How to work with mocks
+/// - create mock classes for any dependencies
+/// - mock methods and properties will always return null
+/// - use stub!
+
+class MockAuthRepository extends Mock implements FakeAuthRepository {
+  /// Mock objects implement all methods in the parent interface
+  /// but they return null by default!
+}
 
 void main() {
   group('AccountScreenController', () {
     test('initial state is AsyncValue.data', () {
-      final authRepository = FakeAuthRepository();
+      final authRepository = MockAuthRepository();
       final controller =
           AccountScreenController(authRepository: authRepository);
+
+      verifyNever(authRepository.signOut);
 
       /// When to use type annotations
       /// - when you declare something (class/ method/ property ..)
@@ -23,6 +36,31 @@ void main() {
       /// AsyncValue.error -> AsyncError
       ///
       /// All subclasses of AsyncValue
+    });
+
+    /// A 'mock' auth repository will let us:
+    /// 1. decide if the signOut() method should succeed or throw an exception
+    /// 2. check if signOut() is actually called
+
+    test('signOut success', () async {
+      final authRepository = MockAuthRepository();
+
+      /// Method stub
+      /// tells the mock what to return/throw when the method is called
+      when(authRepository.signOut).thenAnswer((_) => Future.value());
+      final controller =
+          AccountScreenController(authRepository: authRepository);
+
+      await controller.signOut();
+      expect(controller.debugState, const AsyncData<void>(null));
+
+      verify(authRepository.signOut).called(1);
+
+      /// expect vs verify
+      ///
+      /// Given our controller(object uder test):
+      /// - use expect() to check the output/state/return value
+      /// - use verify() to check the behavior
     });
   });
 }
